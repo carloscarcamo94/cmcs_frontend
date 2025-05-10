018059db985b40afc9b7e693500a68a61b7ede83
Carlos Manuel Cárcamo Segovia <carloscarcamo94@hotmail.com>
2025-05-09 20:59:06 -0600

    Primera versión del Frontend
    Este commit contiene la primera versión del Frontend

---
+++ carcamosegovia_frontend/src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+
+createApp(App).mount('#app')
